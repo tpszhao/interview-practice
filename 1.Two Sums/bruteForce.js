ed88e46fd1804c3a9880fec46a62faec3c6da0d4
tpszhao <tonyzhao111@gmail.com>
2021-01-12 21:59:30 -0800

    renamed function name

--- 1.Two Sums/bruteForce.js
+++ 1.Two Sums/bruteForce.js
@@ -1,7 +1,4 @@
-const sampleArray = [1, 2, 3, 4, 5];
-const sampleTarget = 9;
-
-const computeTwoSums = (list, target) => {
+const twoSum = (list, target) => {
   const length = list.length;
   for (let start = 0; start < length; start++) {
     for (let end = start + 1; end < length; end++) {
